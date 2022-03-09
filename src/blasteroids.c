377bd04cc0cc3d93d08bd81ddc9ee27809c0017b
Felix Black <fblack16@posteo.de>
2022-03-09 21:40:25 +0100

    Added Point2D, and a Spaceship struct. Added Spaceship to the GameState. Updated the window redraw to render the spaceship.

--- src/blasteroids.c
+++ src/blasteroids.c
@@ -2,10 +2,23 @@
 #include <string.h>
 #include <errno.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_color.h>
+#include <allegro5/allegro_primitives.h>
 
 #define DISPLAY_WIDTH 640
 #define DISPLAY_HEIGHT 480
+#define DISPLAY_OFFSET_X DISPLAY_WIDTH / 2
+#define DISPLAY_OFFSET_Y DISPLAY_HEIGHT / 2
 #define FPS 30
+
+#define SPACESHIP_SCALE_FACTOR 20.0f
+#define SPACESHIP_COLOR "hotpink"
+#define SPACESHIP_VERTICES { \
+	{ .x = -0.5f, .y = -0.5f }, \
+	{ .x = 0.5f, .y = 0.0f }, \
+	{ .x = -0.5f, .y = 0.5f }, \
+}
+#define SPACESHIP_LINE_THICKNESS 2.0f
 
 struct keystate {
 	unsigned int is_pressed: 1;
@@ -13,16 +26,32 @@ struct keystate {
 };
 typedef struct keystate KeyState;
 
+struct point_2d {
+	float x;
+	float y;
+};
+typedef struct point_2d Point2D;
+
+struct spaceship {
+	Point2D position;
+	float heading;
+	ALLEGRO_COLOR color;
+	Point2D vertices[3];
+};
+typedef struct spaceship Spaceship;
+
 struct game_state {
 	ALLEGRO_DISPLAY* display_ptr;
 	ALLEGRO_EVENT_QUEUE* event_queue_ptr;
 	ALLEGRO_TIMER* timer_ptr;
 	KeyState key_states[ALLEGRO_KEY_MAX];
+	Spaceship spaceship;
 };
 typedef struct game_state GameState;
 
 void error(char* message);
 void initialize_allegro();
+void initialize_allegro_primitives();
 void initialize_keyboard();
 ALLEGRO_DISPLAY* initialize_display();
 ALLEGRO_EVENT_QUEUE* initialize_event_queue();
@@ -31,10 +60,16 @@ GameState initialize_game_state();
 void destroy_game_state(GameState* game_state_ptr);
 bool is_pressed_or_needs_processing(KeyState key_states[], int keycode);
 void unset_needs_processing(KeyState key_states[]);
+void add_point_2d(Point2D* self, Point2D* other);
+void add_point_2d(Point2D* self, Point2D* other);
+void draw_line(Point2D* start, Point2D* end, ALLEGRO_COLOR color, float thickness);
+Spaceship initialize_spaceship();
+void draw_spaceship(Spaceship* spaceship);
 
 int main(int argc, char** argv) {
 
 	initialize_allegro();
+	initialize_allegro_primitives();
 	initialize_keyboard();
 
 	GameState game_state = initialize_game_state();
@@ -58,6 +93,7 @@ int main(int argc, char** argv) {
 		double start_time = al_get_time();
 		switch (event.type) {
 			case ALLEGRO_EVENT_TIMER:
+				// Update game objects
 				if (is_pressed_or_needs_processing(game_state.key_states, ALLEGRO_KEY_R) && red < 255) red++;
 				if (is_pressed_or_needs_processing(game_state.key_states, ALLEGRO_KEY_G) && green < 255) green++;
 				if (is_pressed_or_needs_processing(game_state.key_states, ALLEGRO_KEY_B) && blue < 255) blue++;
@@ -92,6 +128,7 @@ int main(int argc, char** argv) {
 		// Problem: the difference in timer ticks only reflects whole frames, but not fractional frames.
 		if (needs_redraw && al_event_queue_is_empty(game_state.event_queue_ptr)) {
 			al_clear_to_color(al_map_rgba(red, green, blue, alpha));
+			draw_spaceship(&game_state.spaceship);
 			al_flip_display();
 			needs_redraw = false;
 		}
@@ -107,6 +144,8 @@ int main(int argc, char** argv) {
 
 	destroy_game_state(&game_state);
 
+	// Shut down primitives addon
+	al_shutdown_primitives_addon();
 	// Uninstall allegro
 	al_uninstall_system();
 }
@@ -118,6 +157,13 @@ void error(char* message) {
 void initialize_allegro() {
 	if (!al_init()) {
 		error("Could not initialize allegro!");
+		exit(1);
+	}
+}
+
+void initialize_allegro_primitives() {
+	if (!al_init_primitives_addon()) {
+		error("Could not initialize allegro_primitives!");
 		exit(1);
 	}
 }
@@ -161,11 +207,13 @@ GameState initialize_game_state() {
 		.display_ptr = initialize_display(),
 		.event_queue_ptr = initialize_event_queue(),
 		.timer_ptr = initialize_timer(),
+		.spaceship = initialize_spaceship(),
 	};
 	KeyState key_state = {.is_pressed = 0, .needs_processing = 0};
 	for (int i = 0; i < ALLEGRO_KEY_MAX; ++i) {
 		game_state.key_states[i] = key_state;
 	}
+
 	return game_state;
 }
 
@@ -187,4 +235,47 @@ void unset_needs_processing(KeyState key_states[]) {
 	for (int i = 0; i < ALLEGRO_KEY_MAX; ++i) {
 		key_states[i].needs_processing = 0;
 	}
+}
+
+void add_point_2d(Point2D* self, Point2D* other) {
+	self->x += other->x;
+	self->y += other->y;
+}
+
+void subtract_point_2d(Point2D* self, Point2D* other) {
+	self->x -= other->x;
+	self->y -= other->y;
+}
+
+void draw_line(Point2D* start, Point2D* end, ALLEGRO_COLOR color, float thickness) {
+	al_draw_line(start->x, start->y, end->x, end->y, color, thickness);
+}
+
+Spaceship initialize_spaceship() {
+	Spaceship spaceship = {
+		.position = { .x = 0.0f, .y = 0.0f },
+		.heading = 0.0f,
+		.color = al_color_name(SPACESHIP_COLOR),
+		.vertices = SPACESHIP_VERTICES,
+	};
+
+	ALLEGRO_TRANSFORM transform;
+	al_identity_transform(&transform);
+	al_scale_transform(&transform, SPACESHIP_SCALE_FACTOR, SPACESHIP_SCALE_FACTOR);
+	al_transform_coordinates(&transform, &spaceship.vertices[0].x, &spaceship.vertices[0].y);
+	al_transform_coordinates(&transform, &spaceship.vertices[1].x, &spaceship.vertices[1].y);
+	al_transform_coordinates(&transform, &spaceship.vertices[2].x, &spaceship.vertices[2].y);
+
+	return spaceship;
+}
+
+void draw_spaceship(Spaceship* spaceship) {
+	ALLEGRO_TRANSFORM transform;
+	al_identity_transform(&transform);
+	al_rotate_transform(&transform, spaceship->heading);
+	al_translate_transform(&transform, spaceship->position.x + (float)DISPLAY_OFFSET_X, spaceship->position.y + (float)DISPLAY_OFFSET_Y);
+	al_use_transform(&transform);
+	draw_line(&spaceship->vertices[0], &spaceship->vertices[1], spaceship->color, SPACESHIP_LINE_THICKNESS);
+	draw_line(&spaceship->vertices[1], &spaceship->vertices[2], spaceship->color, SPACESHIP_LINE_THICKNESS);
+	draw_line(&spaceship->vertices[2], &spaceship->vertices[0], spaceship->color, SPACESHIP_LINE_THICKNESS);
 }
