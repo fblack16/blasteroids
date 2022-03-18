da7fe157ecc03ee4177b74a2c26773d0cef12458
Felix Black <fblack16@posteo.de>
2022-03-18 12:59:50 +0100

    Fixed conversion functions for coordinates and fixed coordinate type in circle to world coordinates. Fixed the initialization for asteroids and spaceship. Changed the y-axis in world coordinates to point upwards. Adapted the drawing function to this change.

--- src/blasteroids.c
+++ src/blasteroids.c
@@ -69,7 +69,7 @@ struct mathematical_coordinates {
 typedef struct mathematical_coordinates MathematicalCoordinates;
 
 struct circle {
-	DisplayCoordinates center;
+	WorldCoordinates center;
 	float radius;
 };
 typedef struct circle Circle;
@@ -134,6 +134,9 @@ void draw_asteroid(Asteroid* asteroid);
 void draw_asteroids(Asteroid asteroids[]);
 void update_asteroid(Asteroid* asteroid, float frame_delta);
 void update_asteroids(Asteroid asteroids[], float frame_delta);
+bool circles_collide(Circle* first, Circle* second);
+WorldCoordinates subtract_world_coordinates(WorldCoordinates* first, WorldCoordinates* second);
+float absolute_value_of_world_coordinates(WorldCoordinates* world_coordinates);
 
 int main(int argc, char** argv) {
 
@@ -145,27 +148,19 @@ int main(int argc, char** argv) {
 	al_register_event_source(game_state.event_queue_ptr, al_get_keyboard_event_source());
 	al_register_event_source(game_state.event_queue_ptr, al_get_timer_event_source(game_state.timer_ptr));
 
-	unsigned char red = 0;
-	unsigned char green = 0;
-	unsigned char blue = 0;
-	unsigned char alpha = 0;
-
 	ALLEGRO_EVENT event;
 
 	bool running = true;
 	bool needs_redraw = true;
 	double frame_delta = 0.0;
 	al_start_timer(game_state.timer_ptr);
+
 	while (running) {
 		al_wait_for_event(game_state.event_queue_ptr, &event);
 
 		double start_time = al_get_time();
 		switch (event.type) {
 			case ALLEGRO_EVENT_TIMER:
-				// Update game objects
-				if (is_pressed_or_needs_processing(game_state.key_states, ALLEGRO_KEY_R) && red < 255) red++;
-				if (is_pressed_or_needs_processing(game_state.key_states, ALLEGRO_KEY_G) && green < 255) green++;
-				if (is_pressed_or_needs_processing(game_state.key_states, ALLEGRO_KEY_B) && blue < 255) blue++;
 				if (is_pressed_or_needs_processing(game_state.key_states, ALLEGRO_KEY_ESCAPE)) running = false;
 
 				update_spaceship(&game_state.spaceship, game_state.key_states, (float)frame_delta);
@@ -200,7 +195,7 @@ int main(int argc, char** argv) {
 		// and would need to adjust the timer queue.
 		// Problem: the difference in timer ticks only reflects whole frames, but not fractional frames.
 		if (needs_redraw && al_event_queue_is_empty(game_state.event_queue_ptr)) {
-			al_clear_to_color(al_map_rgba(red, green, blue, alpha));
+			al_clear_to_color(al_color_name("black"));
 			draw_spaceship(&game_state.spaceship);
 			draw_asteroids(game_state.asteroids);
 			al_flip_display();
@@ -316,16 +311,6 @@ void unset_needs_processing(KeyState key_states[]) {
 	}
 }
 
-void add_point_2d(Position* self, Position* other) {
-	self->x += other->x;
-	self->y += other->y;
-}
-
-void subtract_point_2d(Position* self, Position* other) {
-	self->x -= other->x;
-	self->y -= other->y;
-}
-
 void draw_line_in_display_coordinates(DisplayCoordinates* start, DisplayCoordinates* end, ALLEGRO_COLOR color, float thickness) {
 	al_draw_line(start->x, start->y, end->x, end->y, color, thickness);
 }
@@ -336,14 +321,14 @@ void draw_line_in_world_coordinates(WorldCoordinates* start, WorldCoordinates* e
 
 Spaceship initialize_spaceship() {
 	Spaceship spaceship = {
-		.world_coordinates = { .x = (float)DISPLAY_OFFSET_X, .y = (float)DISPLAY_OFFSET_Y },
+		.world_coordinates = { .x = 0.0f, .y = 0.0f },
 		.heading = 0.0f,
 		.color = al_color_name(SPACESHIP_COLOR),
 		.vertices = SPACESHIP_VERTICES,
 		.hitbox = {
 			.center = { 
-				.x = (float)DISPLAY_OFFSET_X, 
-				.y = (float)DISPLAY_OFFSET_Y
+				.x = 0.0f, 
+				.y = 0.0f,
 			},
 			.radius = 1.0f * SPACESHIP_SCALE_FACTOR,
 		}
@@ -360,10 +345,11 @@ Spaceship initialize_spaceship() {
 }
 
 void draw_spaceship(Spaceship* spaceship) {
+	DisplayCoordinates display_coordinates = map_world_coordinates_to_display_coordinates(&spaceship->world_coordinates);
 	ALLEGRO_TRANSFORM transform;
 	al_identity_transform(&transform);
-	al_rotate_transform(&transform, spaceship->heading);
-	al_translate_transform(&transform, spaceship->world_coordinates.x, spaceship->world_coordinates.y);
+	al_rotate_transform(&transform, -spaceship->heading); // need to take the negative of heading since drawing is in display coordinates
+	al_translate_transform(&transform, display_coordinates.x, display_coordinates.y);
 	al_use_transform(&transform);
 	draw_line_in_display_coordinates(&spaceship->vertices[0], &spaceship->vertices[1], spaceship->color, SPACESHIP_LINE_THICKNESS);
 	draw_line_in_display_coordinates(&spaceship->vertices[1], &spaceship->vertices[2], spaceship->color, SPACESHIP_LINE_THICKNESS);
@@ -380,10 +366,10 @@ void update_spaceship(Spaceship* spaceship, KeyState key_states[], float frame_d
 		spaceship->world_coordinates.y -= (float)sin(spaceship->heading) * (1.0f + frame_delta);
 	}
 	if (is_pressed_or_needs_processing(key_states, LEFT)) {
-		spaceship->heading = (spaceship->heading - 0.1f * (1.0f + frame_delta));
+		spaceship->heading = (spaceship->heading + 0.1f * (1.0f + frame_delta));
 	}
 	if (is_pressed_or_needs_processing(key_states, RIGHT)) {
-		spaceship->heading = (spaceship->heading + 0.1f * (1.0f + frame_delta));
+		spaceship->heading = (spaceship->heading - 0.1f * (1.0f + frame_delta));
 	}
 
 	spaceship->hitbox.center.x = spaceship->world_coordinates.x;
@@ -393,30 +379,31 @@ void update_spaceship(Spaceship* spaceship, KeyState key_states[], float frame_d
 }
 
 void map_world_coordinates_to_screen(WorldCoordinates* world_coordinates, float angle) {
-	if (world_coordinates->x < 0.0f) world_coordinates->x = (float)DISPLAY_WIDTH;
-	if (world_coordinates->x > (float)DISPLAY_WIDTH) world_coordinates->x = 0.0f;
-	if (world_coordinates->y < 0.0f) world_coordinates->y = (float)DISPLAY_HEIGHT;
-	if (world_coordinates->y > (float)DISPLAY_HEIGHT) world_coordinates->y = 0.0f;
+	if (world_coordinates->x < -(float)DISPLAY_OFFSET_X) world_coordinates->x = (float)DISPLAY_OFFSET_X;
+	if (world_coordinates->x > (float)DISPLAY_OFFSET_X) world_coordinates->x = -(float)DISPLAY_OFFSET_X;
+	if (world_coordinates->y < -(float)DISPLAY_OFFSET_Y) world_coordinates->y = (float)DISPLAY_OFFSET_Y;
+	if (world_coordinates->y > (float)DISPLAY_OFFSET_Y) world_coordinates->y = -(float)DISPLAY_OFFSET_Y;
 }
 
 void map_display_coordinates_to_screen(DisplayCoordinates* display_coordinates, float angle) {
-	WorldCoordinates world_coordinates = map_display_coordinates_to_world_coordinates(display_coordinates);
-	map_world_coordinates_to_screen(&world_coordinates, angle);
-	*display_coordinates = map_world_coordinates_to_display_coordinates(&world_coordinates);
+	if (display_coordinates->x < 0.0f) display_coordinates->x = (float)DISPLAY_WIDTH;
+	if (display_coordinates->x > (float)DISPLAY_WIDTH) display_coordinates->x = 0.0f;
+	if (display_coordinates->y < 0.0f) display_coordinates->y = (float)DISPLAY_HEIGHT;
+	if (display_coordinates->y > (float)DISPLAY_HEIGHT) display_coordinates->y = 0.0f;
 }
 
 WorldCoordinates map_display_coordinates_to_world_coordinates(DisplayCoordinates* display_coordinates) {
 	WorldCoordinates world_coordinates = {
-		.x = display_coordinates->x + (float)DISPLAY_OFFSET_X,
-		.y = display_coordinates->y + (float)DISPLAY_OFFSET_Y,
+		.x = display_coordinates->x - (float)DISPLAY_OFFSET_X,
+		.y = -display_coordinates->y - (float)DISPLAY_OFFSET_Y,
 	};
 	return world_coordinates;
 }
 
 DisplayCoordinates map_world_coordinates_to_display_coordinates(WorldCoordinates* world_coordinates) {
 	DisplayCoordinates display_coordinates = {
-		.x = world_coordinates->x - (float)DISPLAY_OFFSET_X,
-		.y = world_coordinates->y - (float)DISPLAY_OFFSET_Y,
+		.x = world_coordinates->x + (float)DISPLAY_OFFSET_X,
+		.y = -world_coordinates->y + (float)DISPLAY_OFFSET_Y,
 	};
 	return display_coordinates;
 }
@@ -439,16 +426,16 @@ DisplayCoordinates map_mathematical_coordinates_to_display_coordinates(Mathemati
 
 MathematicalCoordinates map_world_coordinates_to_mathematical_coordinates(WorldCoordinates* world_coordinates) {
 	MathematicalCoordinates mathematical_coordinates = {
-		.x = world_coordinates->x - (float)DISPLAY_OFFSET_X,
-		.y = world_coordinates->y - (float)DISPLAY_OFFSET_Y,
+		.x = world_coordinates->x + (float)DISPLAY_OFFSET_X,
+		.y = world_coordinates->y + (float)DISPLAY_OFFSET_Y,
 	};
 	return mathematical_coordinates;
 }
 
 WorldCoordinates map_mathematical_coordinates_to_world_coordinates(MathematicalCoordinates* mathematical_coordinates) {
 	WorldCoordinates world_coordinates = {
-		.x = mathematical_coordinates->x + (float)DISPLAY_OFFSET_X,
-		.y = mathematical_coordinates->y + (float)DISPLAY_OFFSET_Y,
+		.x = mathematical_coordinates->x - (float)DISPLAY_OFFSET_X,
+		.y = mathematical_coordinates->y - (float)DISPLAY_OFFSET_Y,
 	};
 	return world_coordinates;
 }
@@ -485,10 +472,11 @@ Asteroid initialize_asteroid() {
 }
 
 void draw_asteroid(Asteroid* asteroid) {
+	DisplayCoordinates display_coordinates = map_world_coordinates_to_display_coordinates(&asteroid->world_coordinates);
 	ALLEGRO_TRANSFORM transform;
 	al_identity_transform(&transform);
 	al_rotate_transform(&transform, asteroid->rotation_angle);
-	al_translate_transform(&transform, asteroid->world_coordinates.x, asteroid->world_coordinates.y);
+	al_translate_transform(&transform, display_coordinates.x, display_coordinates.y);
 	al_use_transform(&transform);
 	draw_line_in_display_coordinates(&asteroid->vertices[0], &asteroid->vertices[1], asteroid->color, ASTEROID_LINE_THICKNESS);
 	draw_line_in_display_coordinates(&asteroid->vertices[1], &asteroid->vertices[2], asteroid->color, ASTEROID_LINE_THICKNESS);
@@ -517,4 +505,23 @@ void update_asteroids(Asteroid asteroids[], float frame_delta) {
 	for (int i = 0; i < ASTEROID_MAX; ++i) {
 		update_asteroid(asteroids + i, frame_delta);
 	}
+}
+
+bool circles_collide(Circle* first, Circle* second) {
+	WorldCoordinates difference_of_centers = subtract_world_coordinates(&first->center, &second->center);
+	float distance_of_centers = absolute_value_of_world_coordinates(&difference_of_centers);
+	float sum_of_radii = first->radius + second->radius;
+	return distance_of_centers < sum_of_radii;
+}
+
+WorldCoordinates subtract_world_coordinates(WorldCoordinates* first, WorldCoordinates* second) {
+	WorldCoordinates difference = {
+		.x = first->x - second->x,
+		.y = first->y - second->y,
+	};
+	return difference;
+}
+
+float absolute_value_of_world_coordinates(WorldCoordinates* world_coordinates) {
+	return sqrt(world_coordinates->x * world_coordinates->x + world_coordinates->y * world_coordinates->y);
 }
